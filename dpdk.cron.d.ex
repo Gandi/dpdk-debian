dccf66d769f7d17dba97b85e871afe2f6cd825f0
Nikita Kozlov <nikita@elyzion.net>
2015-07-16 14:57:17 +0200

    fix build for ivy and sandy bridge

--- dpdk.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the dpdk package
-#
-0 4	* * *	root	[ -x /usr/bin/dpdk_maintenance ] && /usr/bin/dpdk_maintenance
--- menu.ex
+++
@@ -1,2 +0,0 @@
-?package(dpdk):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="dpdk" command="/usr/bin/dpdk"
